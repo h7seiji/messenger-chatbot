e8137bbbd8aa7a67392dfda1f713be6663a9c423
h7seiji <helder.costa@usp.br>
2019-04-15 17:42:21 -0300

    add logs

--- index.js
+++ index.js
@@ -159,6 +159,7 @@ function callSendAPI(sender_psid, response) {
     "json": request_body
   }, (err, res, body) => {
     if (!err) {
+      console.log(request_body.message.attachment.payload.elements[0])
       console.log('message sent!')
     } else {
       console.error("Unable to send message:" + err);
