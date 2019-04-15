26f0b7e814594b0b1e685c83666027b302a8668a
h7seiji <helder.costa@usp.br>
2019-04-15 19:56:11 -0300

    test some stuff

--- index.js
+++ index.js
@@ -98,19 +98,33 @@ async function handleMessage(sender_psid, received_message) {
           "elements": [{
             "title": "Olá, bem-vindo à experiência Real2U. Veja alguns de nossos produtos.",
             "subtitle": "Escolha um dos aplicativos abaixo.",
-            "buttons": []
+            "buttons": [{
+                "type": "web_url",
+                "url": "http://bit.ly/real2u-centauro",
+                "title": "Centauro"
+              },
+              {
+                "type": "web_url",
+                "url": "http://bit.ly/real2u-livo",
+                "title": "LIVO"
+              },
+              {
+                "type": "web_url",
+                "url": "http://bit.ly/real2u-persiana",
+                "title": "Persiana"
+              }]
           }]
         }
       }
     };
 
-    const list = await callChatbotApi(response);
+    // const list = await callChatbotApi(response);
 
     // response.attachment.payload.elements[0].buttons = [];
     // response.attachment.payload.elements[0].buttons.push(list);
 
     // Send the response message
-    callSendAPI(sender_psid, list);
+    callSendAPI(sender_psid, response);
 
   } else if (received_message.attachments) {
     // Get the URL of the message attachment
